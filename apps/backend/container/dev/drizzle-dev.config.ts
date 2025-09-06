0f6cedacb2c4f40fe3c0805b13fa8d4f86d6fdc3
PoRi <1960825664@qq.com>
2025-09-06 15:49:23 +0800

    fix

---
+++ apps/backend/container/dev/drizzle-dev.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "drizzle-kit";
+import { config } from "dotenv";
+
+config({ path: ".env" });
+
+export default defineConfig({
+	out: process.env.DRIZZLE_OUT || "./drizzle",
+	schema: process.env.DRIZZLE_SCHEMA || "src/db/schema/index.ts",
+	dialect: "postgresql",
+	dbCredentials: {
+		url: process.env.DATABASE_URL || "",
+	},
+});
