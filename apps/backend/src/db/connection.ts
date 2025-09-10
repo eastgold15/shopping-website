d7773dca6734d461b7821a48a018de4b3aec9a97
PoRi <1960825664@qq.com>
2025-09-10 14:50:55 +0800

    !依赖

--- apps/backend/src/db/connection.ts
+++ apps/backend/src/db/connection.ts
@@ -1,13 +1,16 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
 
+import 'dotenv/config';
+import { drizzle } from 'drizzle-orm/node-postgres';
 import * as schema from "./schema/index";
 
-// PostgreSQL 客户端实例
-const client = postgres(process.env.DATABASE_URL!);
-
-// 创建 Drizzle ORM 实例
-export const db = drizzle(client, {
-	schema,
-	casing: "snake_case",
+// You can specify any property from the node-postgres connection options
+const db = drizzle({
+  connection: {
+    connectionString: process.env.DATABASE_URL || '',
+  },
+  schema,
 });
+
+export {
+  db
+};
